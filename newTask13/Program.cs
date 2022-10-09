0af86eda1c7b1e630532a41c9d489dd077de1f8f
Anastasia <kazantseva_nastya_95@mail.ru>
2022-10-10 01:03:51 +0500

    MathTask13

---
+++ newTask13/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 13: Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей
+// цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int thirdNum = 0;
+
+if (num < 100)
+{
+Console.WriteLine($"Третьего числа не обнаружено");
+return;
+}
+
+else 
+if (num > 100)
+{
+  while(num>1000)
+  {
+    num = num / 10;
+  }
+  thirdNum = num % 10;
+}
+Console.WriteLine($"Третья цифра числа {num} - {thirdNum}");
