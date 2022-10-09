440b04f9e99e8b6e127994ce16c6f6655219717e
Anastasia <kazantseva_nastya_95@mail.ru>
2022-10-10 00:46:47 +0500

    MathTask10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -5,17 +5,36 @@
 // 782 -> 8
 // 918 -> 1
 
+// нематематическое решение
+// Console.Clear();
+// Console.Write("Введите трехзначное число: ");
+// int num = int.Parse(Console.ReadLine());
+// string numText = Convert.ToString (num);
+// if (numText.Length < 3 | numText.Length > 3)
+// {
+//   Console.WriteLine($"Число {num} не соответствует заданному");
+// }
+// else
+// {
+//   Console.WriteLine($"Вторая цифра вашего числа {num} - {numText[1]}");
+// }
+
+// математическое решение
+
 Console.Clear();
 Console.Write("Введите трехзначное число: ");
 int num = int.Parse(Console.ReadLine());
-string numText = Convert.ToString (num);
-if (numText.Length < 3 | numText.Length > 3)
+int secondNumber = 0;
+if (num < 100)
+{
+  Console.WriteLine($"Число {num} не соответствует заданному");
+  return;
+}
+else
+if (num > 1000)
 {
   Console.WriteLine($"Число {num} не соответствует заданному");
 }
-else
-{
-  Console.WriteLine($"Вторая цифра вашего числа {num} - {numText[1]}");
+else{
+  Console.WriteLine($"Вторая цифра вашего числа {num} - {num / 10 % 10}");
 }
-
-
